e8d93d81412c9ed2675687afcf9a78de2bd161a4
Lwc2001 <1403371334@qq.com>
2024-01-15 15:33:53 +0800

    [修改] 完善线程池的初始化

---
+++ threadPool/threadPool.c
@@ -0,0 +1,133 @@
+#include "threadPool.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define DEFAULT_MIN_THREADS 5
+#define DEFAULT_MAX_THREADS 10
+#define DEFAULT_QUEUE_CAPACITY 100
+
+enum STATUS_CODE
+{
+    ON_SUCCESS,
+    NULL_PTR,
+    MALLOC_ERROR,
+    INVALID_ACCESS,
+    UNKNOWN_ERROR,
+};
+
+void * threadHander(void *arg)
+{
+    pthread_exit(NULL);
+}
+
+
+
+/* 线程池初始化 */
+int threadPoolInit(threadpool_t *pool, int minThreads, int maxThreads, int queueCapacity)
+{
+    if (pool == NULL)
+    {
+        return NULL_PTR;
+    }
+    
+    do 
+    {
+        /* 判断合法性 */
+        if (minThreads <= 0 || maxThreads <= 0 || minThreads >= maxThreads)
+        {
+            minThreads = DEFAULT_MIN_THREADS;
+            maxThreads = DEFAULT_MAX_THREADS;
+        }
+        /* 更新线程池属性 */
+        pool->minThreads = minThreads;
+        pool->maxThreads = maxThreads;
+
+        /* 判断合法性 */
+        if (queueCapacity <= 0)
+        {
+            queueCapacity = DEFAULT_QUEUE_CAPACITY;
+        }
+        /* 更新线程池 任务队列属性 */
+        pool->queueCapacity = queueCapacity;
+        pool->queueFront = 0;
+        pool->queueRear = 0;
+        pool->queueSize = 0;
+        pool->taskQueue = (task_t *)malloc(sizeof(task_t) * pool->queueCapacity);
+        if (pool->taskQueue == NULL)
+        {
+            perror("malloc error");
+            break;
+        }
+        memset(pool->taskQueue, 0, sizeof(task_t) * pool->queueCapacity);
+
+
+        /* 为线程ID分配堆空间 */
+        pool->threadIds = (pthread_t *)malloc(sizeof(pthread_t) * pool->maxThreads);
+        if (pool->threadIds == NULL)
+        {
+            perror("malloc error");
+            break;
+        }
+        /* 清除脏数据 */
+        memset(pool->threadIds, 0, sizeof(pthread_t) * pool->maxThreads);
+
+        int ret = 0;
+        /* 创建线程 */
+        for (int idx = 0; idx < pool->minThreads; idx++)
+        {
+            /* 如果线程ID号为0. 那个这个位置可以用. */
+            if (pool->threadIds[idx] == 0)
+            {
+                ret = pthread_create(&(pool->threadIds[idx]), NULL, threadHander, NULL);
+                if (ret != 0)
+                {
+                    perror("thread create error");
+                    break;
+                }
+            }
+        }
+        /* 此ret是创建线程函数的返回值 */
+        if (ret != 0)
+        {
+            break;
+        }
+
+        return ON_SUCCESS;
+    } while(0);
+    
+    /* 程序执行到这个地方, 上面一定有失败. */
+
+    /* 回收堆空间 */
+    if (pool->taskQueue != NULL)
+    {
+        free(pool->taskQueue);
+        pool->taskQueue = NULL;
+    }
+
+    /* 回收线程资源 */
+    for (int idx = 0; idx < pool->minThreads; idx++)
+    {
+        if (pool->threadIds[idx] != 0)
+        {
+            pthread_join(pool->threadIds[idx]);
+        }
+    }
+
+    
+    if (pool->threadIds != NULL)
+    {
+        free(pool->threadIds);
+        pool->threadIds = NULL;
+    }
+
+    return UNKNOWN_ERROR;
+}
+
+/* 线程池的销毁 */
+int threadDetory(threadpool_t *pool)
+{
+    
+}
+
--- threadPool/threadPool.h
+++ threadPool/threadPool.h
@@ -3,20 +3,45 @@
 
 #include <pthread.h>
 
+typedef struct task_t
+{
+    /* 回调函数 */
+    void *(woker_Hander)(void *arg);
+    /* 参数 */
+    void *arg;
+}task_t;
 
 /* 线程池结构体 */
 typedef struct threadpool_t
 {
-    /* 线程池中的线程 */
-    pthread_t *threadID;
-    
-    /* 最小线程数 */
-    int minThread;
+    /* 任务队列 */
+    task_t * taskQueue;
+    /* 队列容量 */
+    int queueCapacity;
+    /* 任务队列的任务数 */
+    int queueSize;
+    /* 任务队列的队头 */
+    int queueFront;
+    /* 任务队列的队尾 */
+    int queueRear;
 
-    /* 最大线程数 */
-    int maxThread;
-    
+    /* 线程池中的线程 */
+    pthread_t *threadIds;
+
+    /* 最小的线程数 */
+    int minThreads;
+    /* 最大的线程数 */
+    int maxThreads;
 }threadpool_t;
+
+/* 线程池的初始化 */
+int threadInit(threadpool_t *pool, int minThreads, int maxThreads, int queueCapacity);
+
+/* 线程池的销毁 */
+int threadDetory(threadpool_t *pool);
+
+
+
 
 
 
